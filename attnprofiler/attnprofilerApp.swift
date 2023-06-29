9ee0cca7874987a80187fbdf5c6b38fdaf535069
ZekeAranyLucas <zeke@aranylucas.com>
2023-06-29 15:11:28 +0200

    Initial Commit

---
+++ attnprofiler/attnprofilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  attnprofilerApp.swift
+//  attnprofiler
+//
+//  Created by Zeke on 29.06.23.
+//
+
+import SwiftUI
+
+@main
+struct attnprofilerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
